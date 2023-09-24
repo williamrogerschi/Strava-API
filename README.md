9cfd89f0e18bca453feb2afe58872a9956177313
williamrogerschi <william.rogers.chi@gmail.com>
2023-09-24 13:20:41 -0500

    committing basic outline of site with profile icon and starts of stats data

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Strava-API
+# Strava-API - Rider Profile
+
 An API using data from Strava to pull rider profile stats/data
+
+## Description
+
+This API will be a rider profile that is taking data from Strava. Strava is an app that lets you upload workout activities. In this case, I'm specifically interested in parsing my ride (bicycle) data. Once uploaded you can keep track of all your ride data including: time, location, route, speed, distance, etc. This social media platform is sort of like the 'facebook' for cyclists.
+
+In this project, I will be pulling my ride data in order to make my own rider profile page. I will show cumumaltive ride data as well as stats on recent rides.
+
+### Getting Started
+
+1. First, I will get the API connections configured - Stravas API requires an access code that changes every 6 hours. In order to avoid re-configuring the connection everytime it expires, I have set up a refresh token in order to automatically fetch the updated access token.√ 
+ 
+ - note, I have the acitivities dataset configured, but will have to still add athlete stats. Find information on the available datasets here: https://developers.strava.com/playground/#/
+
+ - Here is the setup for the Strava API: https://developers.strava.com/docs/getting-started/#curl 
+2. Second, I will begin to visualize my wireframe to get a sense of what I want my profile page to look like, and what CSS elements I will need to make this happen.
+3. Third, I will work on my JS logic to start pulling the data necessary from the API(s).
+4. Lastly, I will begin to work with my CSS to make my wireframe vision complete.
+
+#### PseudoCode
+
+First I need to gather all the necessary APIs (w/ links) and define them as variables - in order to fetch all the necessary data.
+I then need to write the logic for each data pull and link them to the correct HTML ids/classes.
+
+Once I get some kind of rythym for pulling some data, I can go back into my HTML and really try to think (based off my wireframe) about the HTML structure and what possible divs, classes, etc. I will need to make my CSS cleaner.
+
+Once I have my HTML in order, along with some key data points - I will then have to consider placement that will make the most sense for someone visiting my profile page (this is where some slight CSS will come into play).
+
+With the page starting to come together, I can look at my JS and try to refine the code to my liking (arrow functions/ternary operators, etc.)
+
+With most of the page data all together, I will then focus lastly on my CSS. Possibly play around with responsive CSS, while making sure my webpage is mobile first.
---
+++ script.js
@@ -0,0 +1,76 @@
+// const button = document.querySelector('#submitButton')
+// const refreshToken = `23f536bd8a5a372dddafcf05df002f9db97388c2`
+
+// button.addEventListener('click', async (event) => {
+//     event.preventDefault()
+//     let getAccessToken = await axios.post(`https://www.strava.com/oauth/token?client_id=114058&client_secret=3a4922753fdd19897799ce35c4eec24a5bbc0cbb&refresh_token=4a99bd37e1926db5c59be661e90bbb0de6d143f2&grant_type=refresh_token`)
+//     console.log(getAccessToken)
+//     let accessToken = getAccessToken.data.access_token
+//     let getData = await axios.get(`https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}`)
+//     console.log(getData)
+// })
+
+// const button = document.querySelector('#submitButton')
+// const refreshToken = `23f536bd8a5a372dddafcf05df002f9db97388c2`
+
+// button.addEventListener('click', async (event) => {
+//     event.preventDefault()
+//     let getAccessToken = await axios.post(`https://www.strava.com/oauth/token?client_id=114058&client_secret=3a4922753fdd19897799ce35c4eec24a5bbc0cbb&refresh_token=4a99bd37e1926db5c59be661e90bbb0de6d143f2&grant_type=refresh_token`)
+//     console.log(getAccessToken)
+//     let accessToken = getAccessToken.data.access_token
+//     let getData = await axios.get(`https://www.strava.com/api/v3/athletes/895809/stats?access_token=${accessToken}`)
+//     console.log(getData)
+// })
+
+const button = document.querySelector('#submitButton')
+const refreshToken = `23f536bd8a5a372dddafcf05df002f9db97388c2`
+const athleteID = ``
+const profilePic = document.querySelector('#profile-img')
+const profileName = document.querySelector('#profile-name')
+
+//mtn. icon variables
+const maxElev = document.querySelector('#max-elev')
+
+//ruler icon variables
+const maxDistance = document.querySelector('#max-distance')
+
+
+
+button.addEventListener('click', async (event) => {
+    event.preventDefault()
+    let getAccessToken = await axios.post(`https://www.strava.com/oauth/token?client_id=114058&client_secret=3a4922753fdd19897799ce35c4eec24a5bbc0cbb&refresh_token=4a99bd37e1926db5c59be661e90bbb0de6d143f2&grant_type=refresh_token`)
+    let accessToken = getAccessToken.data.access_token
+    let getAthlete = await axios.get(`https://www.strava.com/api/v3/athlete?access_token=${accessToken}`)
+    let athleteID = getAthlete.data.id
+    let getStats = await axios.get(`https://www.strava.com/api/v3/athletes/${athleteID}/stats?access_token=${accessToken}`)
+    let getActivities = await axios.get(`https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}`)
+    //getting profile/athlete data
+    console.log(getAthlete)
+    //pulling stats
+    console.log(getStats)
+    //activities
+    console.log(getActivities)
+
+
+    //trying to get profile image to load when button is clicked (for now)
+
+    let profPic = getAthlete.data.profile
+    profilePic.innerHTML = `<img src=${profPic}>`
+    let firstName = getAthlete.data.firstname
+    let lastName = getAthlete.data.lastname
+    profileName.innerHTML = `${firstName} ${lastName}`
+
+    //pulling in max elev to our mtn icon
+    let mElev = Math.round(getStats.data.biggest_climb_elevation_gain * 3.28084)
+    maxElev.innerHTML = `Max Elevation Gain : ${mElev}ft.`
+
+    //pulling in longest ride distance to our ruler icon
+    let mDistance = Math.round(getStats.data.biggest_ride_distance * 0.00062137121212121 * 100) / 100
+    maxDistance.innerHTML = `Max Ride Distance: ${mDistance} miles`
+})
+
+
+
+
+
+
