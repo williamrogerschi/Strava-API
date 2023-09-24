c83f27bf2d8de364231ae26c871f7f5df2a21196
williamrogerschi <william.rogers.chi@gmail.com>
2023-09-24 14:41:43 -0500

    added heatmap

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-// const button = document.querySelector('#submitButton')
-// const refreshToken = `23f536bd8a5a372dddafcf05df002f9db97388c2`
-
-// button.addEventListener('click', async (event) => {
-//     event.preventDefault()
-//     let getAccessToken = await axios.post(`https://www.strava.com/oauth/token?client_id=114058&client_secret=3a4922753fdd19897799ce35c4eec24a5bbc0cbb&refresh_token=4a99bd37e1926db5c59be661e90bbb0de6d143f2&grant_type=refresh_token`)
-//     console.log(getAccessToken)
-//     let accessToken = getAccessToken.data.access_token
-//     let getData = await axios.get(`https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}`)
-//     console.log(getData)
-// })
-
-// const button = document.querySelector('#submitButton')
-// const refreshToken = `23f536bd8a5a372dddafcf05df002f9db97388c2`
-
-// button.addEventListener('click', async (event) => {
-//     event.preventDefault()
-//     let getAccessToken = await axios.post(`https://www.strava.com/oauth/token?client_id=114058&client_secret=3a4922753fdd19897799ce35c4eec24a5bbc0cbb&refresh_token=4a99bd37e1926db5c59be661e90bbb0de6d143f2&grant_type=refresh_token`)
-//     console.log(getAccessToken)
-//     let accessToken = getAccessToken.data.access_token
-//     let getData = await axios.get(`https://www.strava.com/api/v3/athletes/895809/stats?access_token=${accessToken}`)
-//     console.log(getData)
-// })
-
 // button.addEventListener('click', async (event) => {}
 
-
-const button = document.querySelector('#submitButton')
+// const button = document.querySelector('#submitButton')
 const refreshToken = `23f536bd8a5a372dddafcf05df002f9db97388c2`
 const athleteID = ``
 const profilePic = document.querySelector('#profile-img')
@@ -36,6 +11,10 @@ const maxElev = document.querySelector('#max-elev')
 
 //ruler icon variables
 const maxDistance = document.querySelector('#max-distance')
+
+//setting up heat map variables
+const cal = new CalHeatmap()
+let hmActivities = []
 
 
 window.addEventListener('load', async (event) => {
@@ -53,14 +32,25 @@ window.addEventListener('load', async (event) => {
     //activities
     console.log(getActivities)
 
-
     //trying to get profile image to load when button is clicked (for now)
-
     let profPic = getAthlete.data.profile
     profilePic.innerHTML = `<img src=${profPic}>`
     let firstName = getAthlete.data.firstname
     let lastName = getAthlete.data.lastname
     profileName.innerHTML = `${firstName} ${lastName}`
+
+    for (x in hmActivities) {
+         let newObj = new Object()
+         let activityTS = hmActivities[x].start_date_local.format('X')
+         newObj.date = parseInt(activityTS)
+         newObj.value = Math.floor(hmActivities[x].elapsed_time/60)
+         hmActivities.push(newObj)
+    }
+
+    cal.paint({})
+    // render('#cal-heatmap')
+
+
 
     //pulling in max elev to our mtn icon
     let mElev = Math.round(getStats.data.biggest_climb_elevation_gain * 3.28084)
@@ -69,6 +59,8 @@ window.addEventListener('load', async (event) => {
     //pulling in longest ride distance to our ruler icon
     let mDistance = Math.round(getStats.data.biggest_ride_distance * 0.00062137121212121 * 100) / 100
     maxDistance.innerHTML = `Max Ride Distance: ${mDistance} miles`
+
+
 
 })
 
